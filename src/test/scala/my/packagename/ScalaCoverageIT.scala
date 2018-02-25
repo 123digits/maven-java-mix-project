f559f10857f7d8f44f6b13774328ba41f565c076
matt <matt@workstation>
2018-02-25 08:51:05 -0500

    Fix scala tests to not test java casses & add download/start/stop actions for mongo for integration testing

--- src/test/scala/my/packagename/ScalaCoverageIT.scala
+++ src/test/scala/my/packagename/ScalaCoverageIT.scala
@@ -7,11 +7,6 @@ import org.junit.Assert
 class ScalaCoverageIT extends AssertionsForJUnit {
 
   @Test
-  def test {
-    Assert.assertTrue(JavaCoverage.myMethod());
-  }
-
-  @Test
   def testScala {
     Assert.assertTrue(ScalaCoverage.myMethod);
   }
--- src/test/scala/my/packagename/ScalaCoverageTest.scala
+++ src/test/scala/my/packagename/ScalaCoverageTest.scala
@@ -7,11 +7,6 @@ import org.junit.Assert
 class ScalaCoverageTest extends AssertionsForJUnit {
 
   @Test
-  def testJava {
-    Assert.assertTrue(JavaCoverage.myMethod());
-  }
-
-  @Test
   def testScala {
     Assert.assertTrue(ScalaCoverage.myMethod);
   }
